0dfa5f57718875c57f6431e6edada408f835d3dd
Oscar Zhang <oscarzhang228@gmail.com>
2024-03-04 21:50:04 -0500

    put profile logo on the landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
+import Landing from "./pages/Landing/Landing";
 
 function App() {
   return (
--- src/pages/Landing.tsx
+++
@@ -1,5 +0,0 @@
-function Landing() {
-  return <div>Hello</div>;
-}
-
-export default Landing;
